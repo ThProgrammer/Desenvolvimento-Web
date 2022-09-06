1a11b098bf5691757c7d4f507c4688cc6fae208a
Thomas Ferreira <thomas.devfs@gmail.com>
2022-09-06 13:27:39 -0300

    Treino de Condições Parte 1
    06/09/2022

---
+++ JavaScript/Condições/nacionalidade.js
@@ -0,0 +1,8 @@
+var pais = "EUA"
+
+if (pais == "Brasil") {
+    console.log("Você é brasileiro!")
+}
+else {
+    console.log("Você é estrangeiro")
+}
---
+++ JavaScript/Condições/velocidade.js
@@ -0,0 +1,10 @@
+var velocidade_media = 49
+
+if (velocidade_media > 50){
+    console.log(`Você foi multado! Velocidade atual: ${velocidade_media}km/h`)
+}
+
+else{
+    console.log("Você está dentro da velocidade permitida! <3")
+}
+console.log("Dirija sempre com segurança!")
